16c85ab37a4d07877247d101847da0a9ff7f84b9
Jacob Luallen <jluallen@fcc-jc.org>
2021-07-15 15:58:51 -0400

    working expand collapse function takes in id and css class toggle

--- portablefuncs.js
+++ portablefuncs.js
@@ -1,8 +1,8 @@
- const navDropDown= (classToModify) => {
-    const getClassToModify = document.querySelectorAll(classToModify);
-    if (getClassToModify.style.display === "none"); {
-        getClassToModify.style.display = "block";
-    } 
-        getClassToModify.style.display = "none";
-    
-}
+
+// Function that will take in an element id and a class (premade in CSS to toggle to a display of block and )
+const navDropDown= (elementIdToToggle, newClass) => {
+    // const getClassToModify = document.getElementsByClassName(classToModify);
+     document.getElementById(elementIdToToggle).classList.toggle(newClass);
+     
+     
+ }
